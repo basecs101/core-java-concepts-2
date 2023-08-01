a133d012023b9ab18476d825f9eb05c505eae1a0
Vikram Gupta <basecs101@gmail.com>
2023-08-01 09:13:31 +0530

    Java 8 features 1. Functional interface 2. Lambda expression 3. static and default methods

--- java-8-features/src/functional/interface1/Driver.java
+++ java-8-features/src/functional/interface1/Driver.java
@@ -1,27 +1,54 @@
 package functional.interface1;
 
-class Test  implements ExampleInterface{
+class Test implements ExampleInterface{
     @Override
     public int randomCalculate(int a, int b) {
         return 0;
     }
+
+    @Override
+    public void print(int result) {
+        ExampleInterface.super.print(result);
+    }
+
+//    @Override
+//    public void print2() {
+//        ExampleInterface.super.print2();
+//    }
 }
 public class Driver {
     public static void main(String[] args) {
-        ExampleInterface instanceUsingAC = new ExampleInterface() {
+
+        ExampleInterface test = new Test();
+        test.randomCalculate(100,200);
+        test.print2();
+        test.print(100);
+
+
+        ExampleInterface instanceUsingAnonymousClass = new ExampleInterface() {
             @Override
             public int randomCalculate(int a, int b) {
                 System.out.println("randomCalculate() method called inside anonymous class!!!");
                 return 0;
             }
+            @Override
+            public void print(int result) {
+                ExampleInterface.super.print(result);
+            }
+
+            @Override
+            public void print2() {
+                ExampleInterface.super.print2();
+            }
         };
-        instanceUsingAC.print(100);
-        instanceUsingAC.randomCalculate(10,20);
+        instanceUsingAnonymousClass.print(100);
+        instanceUsingAnonymousClass.print2();
+        instanceUsingAnonymousClass.randomCalculate(10,20);
 
 
         ExampleInterface instanceUsingLambdaExp = (a, b) -> {
             System.out.println("Lambda called");
-            return 0;
+            return a*b;
         };
 
         /*
@@ -35,7 +62,7 @@ public class Driver {
          */
 
         instanceUsingLambdaExp.print(200);
-        System.out.println("Return value : " + instanceUsingLambdaExp.randomCalculate(100,200));
+        System.out.println("Return value : " + instanceUsingLambdaExp.randomCalculate(10,20));
 
 
     }
--- java-8-features/src/functional/interface1/ExampleInterface.java
+++ java-8-features/src/functional/interface1/ExampleInterface.java
@@ -1,5 +1,8 @@
 package functional.interface1;
 
+//functional interface is an interface that has at most one abstract method -> exhibits only one feature.
+//it can have any number static methods
+//it can also have any number of default methods
 @FunctionalInterface
 public interface ExampleInterface {
     int randomCalculate(int a, int b);//method declaration for abstract method
@@ -7,4 +10,17 @@ public interface ExampleInterface {
     default void print(int result) {
         System.out.println(result);
     }//default method is a concrete method that written in functional interface.
+
+    default void print2() {
+        System.out.println("inside print2 method");
+    }
+
+    static void showMsg(String msg) {
+        System.out.println(msg);
+    }
+
+    static void showMsg2(String msg) {
+        System.out.println(msg);
+
+    }
 }
